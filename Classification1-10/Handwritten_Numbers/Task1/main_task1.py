a0ed6f9df5b3c89d45cfdf9c03506b5e225ea43a
HenrikStorli <henrisst@stud.ntnu.no>
2021-04-15 17:08:19 +0200

    Template for main created

---
+++ Classification1-10/Handwritten_Numbers/Task1/main_task1.py
@@ -0,0 +1,32 @@
+from classifier import *
+
+# Import data from file
+    # matrix: Full training set
+    # matrix: Full test set
+    # vector: Training lables
+    # vector: Test labels
+ a,s,d,f = from_file()
+
+# Split training set and test set into smaller sets
+split_function()
+
+# Classify
+NN_classifier(test_set, training_set, class_vector_training_set)
+
+## Confusion matrix
+
+## Error rate
+
+
+# 1b)
+
+# Find some of the misclassified pictures
+
+# Plot some of the misclassified pictures
+
+# Finds some correctly classified pictures
+
+# Plot some correctly classified pictures
+
+
+
