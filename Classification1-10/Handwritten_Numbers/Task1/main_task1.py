3e190fed783ad1d44bfd9e3618d8f0a0eb9724ba
HenrikStorli <henrisst@stud.ntnu.no>
2021-04-19 01:02:38 +0200

    Deleted mat_files

--- Classification1-10/Handwritten_Numbers/Task1/main_task1.py
+++ Classification1-10/Handwritten_Numbers/Task1/main_task1.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:7eecac3a0d611c9d8b9fbeb33a8315aef43da1666f212509adb1bed9a0188d3f
-size 2000
+oid sha256:5d2975ba0c4dfbb292f4dc35c432f2c4654c84ce811b8bc7c33cc7bfdc5deb9f
+size 1998
