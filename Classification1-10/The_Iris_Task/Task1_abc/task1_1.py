886682097878f7faaf81143c2cfab84815f7b867
inguna <inguna7020@gmail.com>
2021-04-30 14:47:31 +0200

    Updated comments

--- Classification1-10/The_Iris_Task/Task1_abc/task1_1.py
+++ Classification1-10/The_Iris_Task/Task1_abc/task1_1.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:022f33d39bce44da1aa207a3a78b8ba85093ce7cb6d9107a890648af1c2c7dc6
-size 3688
+oid sha256:46d8116c66d70aa741d8fc1de4387ddca66e9176b56a948b3f024d8c05a08d13
+size 2766
